d96f34601a5af42462ba1235353c7773a7be45ca
GHOSCHT <GHOSCHT@users.noreply.github.com>
2020-06-28 20:25:23 +0000

    deploy: 54976c3d800b465b35ee0ab404d8656f030a484d

---
+++ precache-manifest.b10a6bcd2e7162c520e75e9f6d47cf5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab227fed0f84f4fe3f5fb855b358a70d",
+    "url": "/material-ui-todo-list/index.html"
+  },
+  {
+    "revision": "2c6d8e20bf2c68978518",
+    "url": "/material-ui-todo-list/static/js/2.b71d398b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/material-ui-todo-list/static/js/2.b71d398b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb1e18eafd706ca8825",
+    "url": "/material-ui-todo-list/static/js/main.b93f1795.chunk.js"
+  },
+  {
+    "revision": "02162de219172b975244",
+    "url": "/material-ui-todo-list/static/js/runtime-main.51935f32.js"
+  }
+]);
