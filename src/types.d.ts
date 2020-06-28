cf18768a4fd9bd92ea57f528111b614cd2ae91ea
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-06-28 16:15:47 +0200

    Add types

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,7 @@ interface Todo {
   text: string;
   complete: boolean;
 }
+
+type ToggleTodo = (selectedTodo: Todo) => void;
+type RemoveTodo = (selectedTodo: Todo) => void;
+type AddTodo = (newTodoText: string) => void;
