eb720367e5499b4f73ae62ea413f11d7ec200746
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-06-28 12:53:32 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { CSSProperties, useState } from "react";
+import { TodoList } from "./TodoList";
+import InitialTodos from "./InitialTodos";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState(InitialTodos);
+
+  const style = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  };
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style as CSSProperties}>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <div className="separator"></div>
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
     </div>
   );
 }
